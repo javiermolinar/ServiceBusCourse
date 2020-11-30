e231143b3ce3ed700e650a7334e4543ed6a29e3d
javiermolinar <javiermolinar@live.com>
2020-11-30 08:11:03 +0100

    Support for passing subscription name

--- MessagingCourse/PublishSubscribe/Subscriber/Subscriber.cs
+++ MessagingCourse/PublishSubscribe/Subscriber/Subscriber.cs
@@ -4,9 +4,18 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
+namespace MessagingCourse
+{
+    public class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://messagingcourse.servicebus.windows.net/;SharedAccessKeyName=course;SharedAccessKey=ramt/bWgACAYJq/S7zjMcjCTNGp6zxjWe3Q6hBot7CI=";
+        const string TopicName = "chat";
+        const string SubscriptionName = "yoursubscription";
 
-const string ServiceBusConnectionString = "Endpoint=sb://messagingcourse.servicebus.windows.net/;SharedAccessKeyName=course;SharedAccessKey=ramt/bWgACAYJq/S7zjMcjCTNGp6zxjWe3Q6hBot7CI=";
-const string TopicName = "chat";
-const string SubscriptionName = "yoursubscription";
-//Subscription client
-ISubscriptionClient subscriptionClient;
+        static async Task Main(string[] args)
+        {
+            ISubscriptionClient subscriptionClient;
+            var subscription = args.Length > 0 ? args[0] : SubscriptionName;           
+        }
+    }
+}
